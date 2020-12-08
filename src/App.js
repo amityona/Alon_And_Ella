45529098574dcde978a1905cc857018c48a5cee9
Nir-Cohen <nircohen911@gmail.com>
2020-12-08 11:12:53 +0200

    fix app js main

--- src/App.js
+++ src/App.js
@@ -7,26 +7,9 @@ import NeedyFood from './components/NeedyFood'
 
 function App() {
   return (
-<<<<<<< HEAD
-    <BrowserRouter>
 
       <div className="App">
-      <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/donor/choose">
-          <DonorChoose />
-        </Route>
-        <Route exact path="/needy/food">
-          <NeedyFood />
-        </Route>
-      </div>
-        </BrowserRouter>
-=======
-    <div className="App">
-<Contact/>
-    </div>
->>>>>>> 753fe2ccaae25d3412cfcd56f111d4e3fbd82c72
+  </div>
   );
 }
 
