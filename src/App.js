2e4baacaaaa03b8469b547b30200d307c8f3f194
Gabi Israel <gabiskabinha@gmail.com>
2020-12-08 11:34:51 +0200

    pages 4,5,6- address, file and finish

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Home from './components/Home';
 import { BrowserRouter, Route } from 'react-router-dom'
 import DonorChoose from './components/DonorChoose';
 import NeedyFood from './components/NeedyFood'
+import DonorFile from './components/DonorFile'
+import AddressDetails from './components/AddressDetails';
+import DonorFinish from './components/DonorFinish'
 
 function App() {
   return (
-<<<<<<< HEAD
     <BrowserRouter>
 
       <div className="App">
@@ -20,13 +22,20 @@ function App() {
         <Route exact path="/needy/food">
           <NeedyFood />
         </Route>
+        <Route exact path="/donor/file">
+          <DonorFile />
+        </Route>
+        <Route exact path="/donor/address-details">
+          <AddressDetails />
+        </Route>
+        <Route exact path="/needy/address-details">
+          <AddressDetails />
+        </Route>
+        <Route exact path="/donor/finish">
+          <DonorFinish />
+        </Route>
       </div>
         </BrowserRouter>
-=======
-    <div className="App">
-<Contact/>
-    </div>
->>>>>>> 753fe2ccaae25d3412cfcd56f111d4e3fbd82c72
   );
 }
 
