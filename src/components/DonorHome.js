0d56a0db1f7657da7a487e2920b5fe589de0fc87
Gabriela Israel <gabisisrael@gmail.com>
2021-02-01 23:30:24 +0200

    donor updates

--- src/components/DonorHome.js
+++ src/components/DonorHome.js
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom"
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "./UI/Button.js";
 import Box from "@material-ui/core/Box";
-import { useHistory } from "react-router-dom";
+import { useHistory, useRouteMatch } from "react-router-dom";
 import Form from "react-bootstrap/Form";
 import { BiDonateHeart } from "react-icons/bi"
+import firebase from './firebase'
 
 const useStyles = makeStyles((theme) => ({
   form: {
@@ -42,19 +44,84 @@ export default function DonorHome() {
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
   const history = useHistory();
+  const match = useRouteMatch()
+  const location = useLocation()
+  const db = firebase.firestore()
 
-  function submitForm() {
-    history.push("/donor/all-orders");
+  useEffect(() => {
+    console.log(location.pathname)
+    console.log(location.state)
+  }, [location])
+
+  function submitForm(id){
+   history.push({
+  pathname: '/donor/all-orders',
+  state: { detail: id }
+})
   }
 
-  function submit() {
+  async function createUser(){
+    db.collection("user").add({
+      name:name,
+      phone:phone,
+      type:"donor",
+      approved:false,
+    })
+  }
+
+   async function submit(e) {
+    e.preventDefault()
     console.log([{ name: name, phone: phone }]);
     if (name && phone) {
-      return submitForm();
+      try {
+        const res = await checkUserDetails(phone)
+        if(res === false) {
+          console.log("not find user, created a new user and waiting to approve")
+        }
+      }catch(error) {
+        console.log("error")
+      }
     } else {
-      alert("יש למלא את כל הפרטים");
+      alert("יש למלא את כל הפרטים")
     }
+    }
+
+    async function checkUserDetails(phone) {
+      if(phone) {
+        try {
+          const foundUser = await db
+          .collection('user')
+          .where("phone", "==", phone)
+          .get()
+          const currentUrl = match.currentUrl
+
+          if(foundUser.empty) {
+            console.log("not find user, create a new user")
+            await createUser()
+            return false
+          }
+foundUser.forEach((doc) => {
+  console.log(`user number: ${doc.id}, details:`, doc.data() )
+  const user = doc.data()
+  if(user) {
+    if(currentUrl === "/donor" && user.role === "manager") {
+      alert(`user ${user.name} is not manager`)
+      return
+    }
+    if(user.approved === false) {
+      alert(`user ${user.name} is not approved yet`)
+      return
+    } else submitForm(doc.id)
+  }else if(!user) {
+    alert(`user data: ${user} is missing`)
   }
+})
+        }catch(error) {}
+      }else {
+        alert("יש למלא פרטים")
+      }
+    }
+
 
   return (
     <div>
@@ -67,7 +134,7 @@ export default function DonorHome() {
       </p>
       <br />
 
-      <Form  noValidate autoComplete="off">
+      <Form  noValidate autoComplete="off" >
         <Form.Group controlId="formName">
           <Form.Control type="text" placeholder="שם" className={classes.form} value={name}
           onChange={(e) => setName(e.target.value)}/>
@@ -101,3 +168,7 @@ export default function DonorHome() {
     </div>
   );
 }
+
+
+//  לשלוף מידע מאיידי כדי להציג ההזמנות הפתוחות שהמתנדב הזה לקח לפי האיידי של אותו מתנדב 
+// להציג כל ההזמנות הפתוחות הקיימות שעוד לא לקחו
